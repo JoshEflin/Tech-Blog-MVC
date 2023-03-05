5d45d5f9a6e86d2b0522109014aca75abcf811ae
ragingpenguin <eflinjh@gmail.com>
2023-03-05 13:09:49 -0500

    set up server skeleton

---
+++ server.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const express = require('express');
+require('dotenv').config();
+
+// import express session and store sequelize connection to new class type
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+// express session and handlebars
+const exphbs = require('express-handlebars');
+
+const router = requier('./controllers');
+const sequelize = require('./config/connection');
+// const helpers = require('./utils/helpers';)
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+//cookies 
+const sess = {
+    secret: process.env.SESSION_SECRET,
+    resave:false,
+    saveuninitialized:true,
+    cookie:{},
+    store: new SequelizeStore({
+        db:sequelize
+    })
+
+};
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(router);
+app.use(session(sess));
+app.use(express.static(path.join(__dirname, 'public')));
+
+sequelize.sync({force:false}).then(() => {
+    app.listen(PORT, () => console.log(` Now Listening on port ${PORT}`));
+});
