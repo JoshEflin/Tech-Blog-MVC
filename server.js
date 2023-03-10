e1d10636360e01f9e4de5526a32fc89cbcda4cb9
ragingpenguin <eflinjh@gmail.com>
2023-03-10 09:40:27 -0500

    reorganized import statements

--- server.js
+++ server.js
@@ -2,26 +2,27 @@ const path = require("path");
 const express = require("express");
 require("dotenv").config();
 
-// import express session and store sequelize connection to new class type
-const session = require("express-session");
-const SequelizeStore = require("connect-session-sequelize")(session.Store);
 // express session and handlebars
+const session = require("express-session");
 const exphbs = require("express-handlebars");
 
 const router = require("./controllers");
+const helpers = require('./utils/helpers')
 const config = require("./config/connection");
-// const helpers = require('./utils/helpers';)
 
 const app = express();
-const hbs = exphbs.create();
 const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create({helpers});
 app.use(session(config.sess));
+
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
+
 app.use(router);
 
 config.sequelize.sync({ force: false }).then(() => {
---
+++ utils/helpers.js
@@ -0,0 +1,5 @@
+module.exports = {
+   testMethod: () => {
+    let post = 'NEW POST HERE'
+   } 
+}
