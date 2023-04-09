598c7fd25366058c126684d8d12715b79e5749e5
ragingpenguin <eflinjh@gmail.com>
2023-04-09 12:52:03 -0400

    fixed req session id for new users

--- controllers/APIRoutes/signup.js
+++ controllers/APIRoutes/signup.js
@@ -9,15 +9,19 @@ router.post('/', async(req,res) =>{
     res.status(400).json({message: "your request to sign up was invalid"})
     return 
   }
-  const userData = await User.create(incoming)
+  const userData = await User.create({
+    username:incoming.username,
+    email: incoming.email,
+    password: incoming.password
+  })
   const plainUser= userData.get({plain:true})
 console.log(userData)
   req.session.save(() => {
-    req.session.user_id = incoming.id;
+    req.session.user_id = userData.id;
     req.session.logged_in = true;
     req.session.username = incoming.username;
 
-    res.status(200).json({ user: plainUser, message: "login Created" });
+    res.status(201).json({ user: plainUser, message: "login Created" });
   })
 })
 
